786fc29aae6f991a1115b0d12ea56c678b4419c4
Oliver Nightingale <oliver.nightingale1@gmail.com>
2012-12-13 11:18:02 +0000

    slightly less naive tokenizer

--- test/tokenizer_test.js
+++ test/tokenizer_test.js
@@ -6,3 +6,31 @@ test("splitting simple strings into tokens", function () {
 
   same(tokens, ['this', 'is', 'a', 'simple', 'string'])
 })
+
+test('handling arrays', function () {
+  var tags = ['foo', 'bar'],
+      tokens = lunr.tokenizer(tags)
+
+  deepEqual(tokens, tags)
+})
+
+test('removing punctuation', function () {
+  var fullStop = 'hello.',
+      innerApostrophe = "it's",
+      trailingApostrophe = "james' ball",
+      exclamationMark = 'stop!',
+      comma = 'first, second and third'
+
+  deepEqual(lunr.tokenizer(fullStop), ['hello'])
+  deepEqual(lunr.tokenizer(innerApostrophe), ["it's"])
+  deepEqual(lunr.tokenizer(trailingApostrophe), ["james'", 'ball'])
+  deepEqual(lunr.tokenizer(exclamationMark), ['stop'])
+  deepEqual(lunr.tokenizer(comma), ['first', 'second', 'and', 'third'])
+})
+
+test('handling multiple white spaces', function () {
+  var testString = 'foo    bar',
+      tokens = lunr.tokenizer(testString)
+
+  deepEqual(tokens, ['foo', 'bar'])
+})
