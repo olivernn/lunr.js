dee23136008c7deb7a7da137da3cf088e408684d
Oliver Nightingale <oliver.nightingale1@gmail.com>
2012-12-14 11:32:43 +0000

    some tweaks to the pipeline object

--- test/pipeline_test.js
+++ test/pipeline_test.js
@@ -8,6 +8,13 @@ test("adding a new item to the pipeline", function () {
   equal(pipeline.size(), 1)
 })
 
+test("adding multiple items to the pipeline in one go", function () {
+  var pipeline = new lunr.Pipeline
+
+  pipeline.add($.noop, $.noop)
+  equal(pipeline.size(), 2)
+})
+
 test("removing an item from the pipeline", function () {
   var pipeline = new lunr.Pipeline,
       fn = $.noop
@@ -36,21 +43,19 @@ test("adding an item to the pipeline after another item", function () {
       fn2 = function () {},
       fn3 = console.log
 
-  pipeline.add(fn1)
-  pipeline.add(fn2)
+  pipeline.add(fn1, fn2)
   pipeline.after(fn1, fn3)
 
   same(pipeline.toArray(), [fn1, fn3, fn2])
 })
 
-test("run mcalls each member of the pipeline for each input", function () {
+test("run calls each member of the pipeline for each input", function () {
   var pipeline = new lunr.Pipeline,
       count1 = 0, count2 = 0
-      fn1 = function () { count1++ },
-      fn2 = function () { count2++ }
+      fn1 = function (token) { count1++ ; return token },
+      fn2 = function (token) { count2++ ; return token }
 
-  pipeline.add(fn1)
-  pipeline.add(fn2)
+  pipeline.add(fn1, fn2)
 
   pipeline.run([1,2,3])
 
@@ -91,4 +96,17 @@ test("run should return the result of running the entire pipeline on each elemen
       fn1 = function (t1) { return t1.toUpperCase() }
   pipeline.add(fn1)
   same(pipeline.run(['a']), ['A'])
+})
+
+test("run should filter out any undefined values at each stage in the pipeline", function () {
+  var pipeline = new lunr.Pipeline,
+      fn2Count = 0,
+      fn1 = function (t) { if (t < 5) return t },
+      fn2 = function (t) { fn2Count++ ; return t }
+
+  pipeline.add(fn1, fn2)
+
+  var output = pipeline.run([0,1,2,3,4,5,6,7,8,9])
+  equal(fn2Count, 5)
+  equal(output.length, 5)
 })
