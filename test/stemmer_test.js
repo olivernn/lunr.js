af993334228785cebda86a2e9b6e8585f627fc99
Oliver Nightingale <oliver.nightingale1@gmail.com>
2012-12-14 11:56:03 +0000

    stop word filter and stemmer

---
+++ test/stemmer_test.js
@@ -0,0 +1,9 @@
+module('stemmer')
+
+test('should stem words correctly', function () {
+  Object.keys(stemmingFixture).forEach(function (testWord) {
+    var expected = stemmingFixture[testWord]
+
+    equal(lunr.stemmer(testWord), expected)
+  })
+})
