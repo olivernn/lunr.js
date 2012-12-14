11eddfc4d30aa266a23fc0ce6ab59790289e783f
Oliver Nightingale <oliver.nightingale1@gmail.com>
2012-12-14 17:17:54 +0000

    perf fixes for the pipeline run

--- perf/index_test.js
+++ perf/index_test.js
@@ -1,12 +1,9 @@
 var suite = new Benchmark.Suite,
-    index = new lunr.Index,
     testDoc = {
       id: 1,
       title: 'Adding story from last story in the sprint',
       body: 'So that I am not confused where the story is going to end up As a user I want the the add a story button from the last story in the sprint to create a story at the top of the backlog and not extend the sprint temporarily the add story button inserts a story at the top of the backlog. "add a new story here" prompts are not shown for stories that are currently in a sprint'
     }
-
-index 
 
 suite.add('index#add', function () {
   var idx = lunr(function () {
