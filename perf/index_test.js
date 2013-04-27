4fe0ae0a21304d03c48c800c28742248b72cb3c7
Oliver Nightingale <oliver.nightingale1@gmail.com>
2013-04-28 00:19:31 +0100

    adding some basic performance tests

--- perf/index_test.js
+++ perf/index_test.js
@@ -1,27 +1,27 @@
-var suite = new Benchmark.Suite,
-    testDoc = {
+(function () {
+  var setup = function () {
+    var testDoc = {
       id: 1,
       title: 'Adding story from last story in the sprint',
       body: 'So that I am not confused where the story is going to end up As a user I want the the add a story button from the last story in the sprint to create a story at the top of the backlog and not extend the sprint temporarily the add story button inserts a story at the top of the backlog. "add a new story here" prompts are not shown for stories that are currently in a sprint'
     }
 
-suite.add('index#add', function () {
-  var idx = lunr(function () {
-    this.field('title', 10)
-    this.field('body')
-  })
+    questionsIdx.version = lunr.version
 
-  idx.add(testDoc)
-})
+    var idx = lunr.Index.load(questionsIdx)
+  }
 
-suite.on('cycle', function (e) {
-  console.log(e.target.name)
-})
+  bench('index#add', function () {
+    idx.add(testDoc)
+  }, { setup: setup })
 
-suite.on('complete', function (e) {
-  suite.forEach(function (s) {
-    console.log(s.name, s.count)
-  })
-})
+  bench('index#search uncommon word', function () {
+    idx.search('checkbox')
+  }, { setup: setup })
 
-suite.run({async: true})
+  bench('index#search common word', function () {
+    idx.search('javascript')
+  }, { setup: setup })
+
+})()
+
