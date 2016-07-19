abdbe2046f641fbff193a05f9d548e0706d6cd0b
Oliver Nightingale <oliver.nightingale1@gmail.com>
2016-07-19 21:31:58 +0000

    Convert stemming test to mocha/chai.

--- test/mocha_helper.js
+++ test/mocha_helper.js
@@ -1,5 +1,13 @@
 var lunr = require('../lunr.js'),
-    assert = require('chai').assert
+    assert = require('chai').assert,
+    fs = require('fs'),
+    path = require('path')
+
+var fixture = function (name) {
+  var fixturePath = path.join('test', 'fixtures', name)
+  return fs.readFileSync(fixturePath)
+}
 
 global.lunr = lunr
 global.assert = assert
+global.fixture = fixture
